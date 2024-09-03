0035fb4e66580244b387001fbdb61ea80c592093
arttu aalto <t1aaar01@students.oamk.fi>
2024-09-03 22:09:30 +0300

    Valmis vkoteht2

--- App.js
+++ App.js
@@ -1,16 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, View, Modal } from 'react-native';
 
 export default function App() {
+  const [show, setShow] = useState(false)
+  const [showText, setShowText] = useState(true)
+
+  const clickText = () => {
+    setShow(true)
+    setShowText(false)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {showText && (
+      <Pressable onPress={clickText}>
+          <Text>Show modal message</Text>
+      </Pressable>
+      )}
+      <Modal
+        transparent={true}
+        visible={show}
+        onRequestClose={() => {
+          setShow(!show);
+          setShowText(true)
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>This is modal...</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() =>{ 
+                setShow(!show); 
+                setShowText(true)
+              }}>
+              <Text></Text>
+              <Text style={styles.textStyle}>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+        
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 200,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    width: 400,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+  
+  shadowOpacity: 0.25,
+  shadowRadius: 4,
+  elevation: 5,
+},
   container: {
     flex: 1,
     backgroundColor: '#fff',
